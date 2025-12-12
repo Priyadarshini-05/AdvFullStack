1b8a1063caaee1f0edaae40518ffd0cda6a5f433
Priyadarshini-05 <priyadarshini.du05@gmail.com>
2025-12-12 11:57:21 +0530

    Updated code again

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ import { connectDB } from './config/db.js';
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({"origin":"*"}));
 app.use(express.json());
 
 app.use('/api', authRoutes);
